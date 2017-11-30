b85ea95dafd032ae2bb5107629b6af88ef5b9a7a
aleksandr_lukovkin <aleksandr_lukovkin@epam.com>
2017-11-30 19:16:20 +0300

    Add part of postprocessing

--- src/main/java/quotes/ProfilingHandlerBeanPostProcessor.java
+++ src/main/java/quotes/ProfilingHandlerBeanPostProcessor.java
@@ -4,6 +4,9 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.lang.Nullable;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,6 +26,15 @@ public class ProfilingHandlerBeanPostProcessor implements BeanPostProcessor {
     @Nullable
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        return null;
+        Class beanClass = map.get(beanName);
+        if (beanClass!=null){
+            return Proxy.newProxyInstance(beanClass.getClassLoader(), beanClass.getInterfaces(), new InvocationHandler() {
+                @Override
+                public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                    return null;
+                }
+            });
+        }
+        return bean;
     }
 }
