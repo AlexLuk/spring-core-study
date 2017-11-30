f3a0096c83cfdc35a90437f21ffd4a88993d4d24
aleksandr_lukovkin <aleksandr_lukovkin@epam.com>
2017-11-30 20:25:11 +0300

    Add infinite loop

--- src/main/java/quotes/Application.java
+++ src/main/java/quotes/Application.java
@@ -7,7 +7,7 @@ import static java.lang.Thread.sleep;
 public class Application {
     public static void main(String[] args) throws InterruptedException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-        for (int i = 0; i < 10; i++) {
+        while (true) {
             sleep(100);
             context.getBean(Quoter.class).sayQuote();
         }
