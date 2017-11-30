93428c76c07210d0f1ec45f5ab4a7457374e4494
aleksandr_lukovkin <aleksandr_lukovkin@epam.com>
2017-11-30 21:17:55 +0300

    Remove unnecessary call

--- src/main/java/quotes/Application.java
+++ src/main/java/quotes/Application.java
@@ -5,6 +5,5 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Application {
     public static void main(String[] args) throws InterruptedException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-        context.getBean(Quoter.class).sayQuote();
     }
 }
