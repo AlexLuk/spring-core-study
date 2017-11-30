d37a06f0cd43b94990042989d92f4c540e29bc65
aleksandr_lukovkin <aleksandr_lukovkin@epam.com>
2017-11-30 19:10:04 +0300

    Add preprocessing

--- src/main/java/quotes/ProfilingHandlerBeanPostProcessor.java
+++ src/main/java/quotes/ProfilingHandlerBeanPostProcessor.java
@@ -4,11 +4,20 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.lang.Nullable;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ProfilingHandlerBeanPostProcessor implements BeanPostProcessor {
+    private Map<String, Class> map = new HashMap<>();
+
     @Nullable
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-        return null;
+        Class<?> beanClass = bean.getClass();
+        if (beanClass.isAnnotationPresent(Profiling.class)) {
+            map.put(beanName, beanClass);
+        }
+        return bean;
     }
 
     @Nullable
