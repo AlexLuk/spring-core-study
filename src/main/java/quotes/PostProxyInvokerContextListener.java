bba7572a9c845c37f4360d79d0fc65858bee0fe0
aleksandr_lukovkin <aleksandr_lukovkin@epam.com>
2017-11-30 20:45:58 +0300

    Add blank PostProxy context listener

---
+++ src/main/java/quotes/PostProxyInvokerContextListener.java
@@ -0,0 +1,12 @@
+package quotes;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+
+public class PostProxyInvokerContextListener implements ApplicationListener<ContextRefreshedEvent> {
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+
+    }
+}
