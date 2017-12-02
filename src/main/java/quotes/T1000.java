f8551c146e5eeefa241f236d8773b680fbe82b60
aleksandr_lukovkin <aleksandr_lukovkin@epam.com>
2017-12-02 11:26:21 +0300

    Add DeprecatedClass annotation Mark TerminatorQuoter as @DeprecatedClass Add T1000 class Add new quote for T1000 Remove sayQuite from main application method

---
+++ src/main/java/quotes/T1000.java
@@ -0,0 +1,8 @@
+package quotes;
+
+public class T1000 extends TerminatorQuoter {
+    @Override
+    public void sayQuote() {
+        System.out.println("I'm liquid");
+    }
+}
--- src/main/java/quotes/TerminatorQuoter.java
+++ src/main/java/quotes/TerminatorQuoter.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import javax.annotation.PostConstruct;
 
 @Profiling
+@DeprecatedClass(newImpl = T1000.class)
 public class TerminatorQuoter implements Quoter {
 
     @InjectRandomInt(min = 2, max = 7)
