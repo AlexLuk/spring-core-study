68c57a4d2c9f79217906a2919fca70b13ee448cd
aleksandr_lukovkin <aleksandr_lukovkin@epam.com>
2017-12-04 12:01:30 +0300

    Add call in for bread methods

--- src/main/java/groovy_example/GroovyAppRunner.java
+++ src/main/java/groovy_example/GroovyAppRunner.java
@@ -5,6 +5,7 @@ import org.springframework.context.support.GenericGroovyApplicationContext;
 public class GroovyAppRunner {
     public static void main(String[] args) {
         GenericGroovyApplicationContext context = new GenericGroovyApplicationContext("context.groovy");
+        Bread bean = context.getBean("myBean",Bread.class);
+        bean.slice();
     }
-
 }
